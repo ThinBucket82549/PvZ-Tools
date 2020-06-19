68174e810f41182144b6f0623a3828e2563424c8
zSupremoz <erickilpatrick572@gmail.com>
2020-06-18 20:20:18 -0400

    Update botchanges.js

--- commands/botchanges.js
+++ commands/botchanges.js
@@ -5,7 +5,7 @@ const ChangelogEmbed = new Discord.MessageEmbed()
     .setColor('#4da43b')
     .setTitle('v1.4.0 Changelogs')
     .setAuthor('PvZ Tools', 'https://i.imgur.com/REUXZUa.png')
-    .setDescription('• pvz.nextkey now tells how many keys have already been distributed.\n• All commands now have a timestamp in the footer\n• Added pvz.eventdata\n• Added an embed for unknown commands.\n• Changed commands pvz.bfnevents and pvz.gw2events to pvz.weeklyevent and pvz.mysteryportal.\n• Added images for BFN Randomizer commands.\n• Changed the Mystery Portal Embed\n• Added Contributors field and link to the support server in pvz.info')
+    .setDescription('• pvz.nextkey now tells how many keys have already been distributed.\n• All commands now have a timestamp in the footer.\n• Added pvz.eventdata\n• Added an embed for unknown commands.\n• Changed commands pvz.bfnevents and pvz.gw2events to pvz.weeklyevent and pvz.mysteryportal.\n• Added images for BFN Randomizer commands.\n• Changed the Mystery Portal Embed.\n• Added Contributors field and link to the support server in pvz.info.')
     .addFields(
         { name: 'All Changelogs', value: 'https://github.com/zSupremoz/PvZ-Tools/wiki/Changelogs' }
     )
