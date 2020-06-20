74428857ac9c1e5c6fd95e6d43f87df2128fdced
zSupremoz <erickilpatrick572@gmail.com>
2020-06-20 10:28:36 -0400

    Update botchanges.js

--- commands/botchanges.js
+++ commands/botchanges.js
@@ -3,9 +3,9 @@ const { version } = require('../config.json');
 
 const ChangelogEmbed = new Discord.MessageEmbed()
     .setColor('#4da43b')
-    .setTitle('v1.4.2 Changelogs')
+    .setTitle('v1.5.0 Changelogs')
     .setAuthor('PvZ Tools', 'https://i.imgur.com/REUXZUa.png')
-    .setDescription('• Garden Warfare 2 Randomizers now feature character images.\n• Updated pvz.help to show more organized information.\n• Updated pvz.info.\n• Added pvz.todo.\n Added a new bot status.')
+    .setDescription('• Garden Warfare 2 Randomizers now feature character images.\n• Updated pvz.help to show more organized information.\n• Updated pvz.info.\n• Added pvz.todo.\n• Added a new bot status.')
     .addFields(
         { name: 'All Changelogs', value: 'https://github.com/zSupremoz/PvZ-Tools/wiki/Changelogs' }
     )
